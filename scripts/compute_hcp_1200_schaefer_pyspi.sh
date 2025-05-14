02c4a5480dff5e3757e41dd536b6a4f967de6cdc
Connor Lane <connor.lane858@gmail.com>
2025-05-14 16:55:14 -0400

    Update compute pyspi script to use gnu parallel

--- scripts/compute_hcp_1200_schaefer_pyspi.sh
+++ scripts/compute_hcp_1200_schaefer_pyspi.sh
@@ -15,18 +15,15 @@ set +a
 
 export OMP_NUM_THREADS=2
 
-spi_list=( $(cat "resources/spi_lists/spi_list_select_300s_147.txt") )
+spi_list_path="resources/spi_lists/spi_list_select_300s_142.txt"
+sub_list_path="resources/subject_lists/hcp_complete_data_867_subject_list.txt"
 
-spis_per_task=30
-spi_start_idx=$(( SLURM_ARRAY_TASK_ID * spis_per_task ))
-num_spis=${#spi_list[@]}
+subs_per_batch=175
+num_total_subs=867
+sub_start_idx=$(( SLURM_ARRAY_TASK_ID * subs_per_batch ))
+sub_stop_idx=$(( sub_start_idx + subs_per_batch ))
+sub_list=$(sed -n "$((sub_start_idx+1)),${sub_stop_idx}p" ${sub_list_path})
 
-for ((ii=0; ii<spis_per_task; ii++)); do
-    spi_idx=$((spi_start_idx + ii))
-    if (( spi_idx >= num_spis )); then
-        break
-    fi
-
-    spi=${spi_list[spi_idx]}
-    uv run python scripts/compute_hcp_1200_schaefer_pyspi.py $spi
-done
+parallel --jobs 64 \
+    uv run --no-sync python scripts/compute_hcp_1200_schaefer_pyspi.py \
+    {1} {2} :::: ${spi_list_path} ::: ${sub_list}
